da4979927bc4876967f87cc557e590ce384617e2
Bartek <bartraw23@gmail.com>
2021-04-01 22:46:51 +0200

    Initialize project with spring boot

---
+++ src/main/java/io/genderrecognition/GenderRecoginitionAppApplication.java
@@ -0,0 +1,13 @@
+package io.genderrecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderRecoginitionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenderRecoginitionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/genderrecognition/GenderRecoginitionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.genderrecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderRecoginitionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
