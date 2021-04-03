4ee1ff5ef12a29666eb6d70a5c6423930e8efe34
Bartek <bartraw23@gmail.com>
2021-04-03 20:12:46 +0200

    Add service for managing resource use in search for name

---
+++ src/main/java/io/genderrecognition/service/RecognizeGenderService.java
@@ -0,0 +1,22 @@
+package io.genderrecognition.service;
+
+import io.genderrecognition.model.Gender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecognizeGenderService {
+    @Autowired
+    private IdentifiedNameService identifiedNameService;
+
+    @Autowired
+    private FileScraper fileScraper;
+
+    public Gender getGender(String name) {
+        Gender gender = identifiedNameService.findByName(name);
+        if (gender == (Gender.UNDEFINED)) {
+            return fileScraper.returnGender(name);
+        }
+        return gender;
+    }
+}
