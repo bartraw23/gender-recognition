18a2ece7dabe02b4947a10152ce1dcb148ae7b7b
Bartek <bartraw23@gmail.com>
2021-04-04 00:55:24 +0200

    Add Initializingbean for DB init and upload

---
+++ src/main/java/io/genderrecognition/service/InitializingDatabaseService.java
@@ -0,0 +1,27 @@
+package io.genderrecognition.service;
+
+import io.genderrecognition.model.BaseState;
+import io.genderrecognition.model.Gender;
+import io.genderrecognition.repository.BaseStateRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitializingDatabaseService implements InitializingBean {
+
+    @Autowired
+    BaseStateRepository baseStateRepository;
+
+    @Autowired
+    private BaseLoaderService baseLoaderService;
+
+    @Async
+    @Override
+    public void afterPropertiesSet() {
+        baseStateRepository.save(new BaseState(Gender.FEMALE, false));
+        baseStateRepository.save(new BaseState(Gender.MALE, false));
+        baseLoaderService.loadNames();
+    }
+}
