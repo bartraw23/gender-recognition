2c252c89cfa4baeaeee5d843cd4a1fe2ecda4faf
Bartek <bartraw23@gmail.com>
2021-04-04 00:51:57 +0200

    Add method to obtain List by Gender from IdentifiedNameRepository

--- src/main/java/io/genderrecognition/repository/IdentifiedNameRepository.java
+++ src/main/java/io/genderrecognition/repository/IdentifiedNameRepository.java
@@ -4,11 +4,14 @@ import io.genderrecognition.model.Gender;
 import io.genderrecognition.model.IdentifiedName;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IdentifiedNameRepository extends CrudRepository<IdentifiedName, Long> {
 
     Optional<Gender> findByName(String name);
+
+    Optional<List<IdentifiedName>> findByGender(Gender gender);
 }
 
 
