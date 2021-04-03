afeda52af174c9b67c3e142ba91503aaecb428ea
Bartek <bartraw23@gmail.com>
2021-04-03 22:20:16 +0200

    Add BaseState entity for tracking upload status

---
+++ src/main/java/io/genderrecognition/model/BaseState.java
@@ -0,0 +1,36 @@
+package io.genderrecognition.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class BaseState {
+    @Id
+    private Gender gender;
+    private Boolean uploaded;
+
+    public BaseState(Gender gender, Boolean uploaded) {
+        this.gender = gender;
+        this.uploaded = uploaded;
+    }
+
+    public BaseState() {
+
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Boolean getUploaded() {
+        return uploaded;
+    }
+
+    public void setUploaded(Boolean uploaded) {
+        this.uploaded = uploaded;
+    }
+}
