2ac2115ff60a7eb84fef2fbb8149aecbd07d0515
Bartek <bartraw23@gmail.com>
2021-04-04 00:57:41 +0200

    Add get for tokens and refactor

--- src/main/java/io/genderrecognition/controller/RecognitionController.java
+++ src/main/java/io/genderrecognition/controller/RecognitionController.java
@@ -1,9 +1,10 @@
 package io.genderrecognition.controller;
 
 import io.genderrecognition.model.Gender;
-import io.genderrecognition.service.BaseLoaderService;
 import io.genderrecognition.service.RecognizeGenderService;
+import io.genderrecognition.service.TokensService;
 import io.genderrecognition.wrapper.PersonWrapper;
+import io.genderrecognition.wrapper.Tokens;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,19 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class RecognitionController {
-    @Autowired
-    private BaseLoaderService baseLoaderService;
 
     @Autowired
     private RecognizeGenderService recognizeGenderService;
 
-    @GetMapping("/loadDB")
-    public void loadDatabase() {
-        baseLoaderService.loadNames();
-    }
+    @Autowired
+    private TokensService tokensService;
 
     @GetMapping("/name")
     public Gender checkGender(@RequestBody PersonWrapper personWrapper) {
         return recognizeGenderService.recognizeGender(personWrapper.getName(), personWrapper.getByAllTokens());
+    }
+
+    @GetMapping("/tokens")
+    public Tokens getTokens() {
+        return tokensService.getTokens();
     }
 }
