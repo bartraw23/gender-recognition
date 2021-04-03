93755fd7b158c833c32c24c3c5cc8b465821be54
Bartek <bartraw23@gmail.com>
2021-04-03 20:07:15 +0200

    Adapt findByName method for optional use

--- src/main/java/io/genderrecognition/service/IdentifiedNameService.java
+++ src/main/java/io/genderrecognition/service/IdentifiedNameService.java
@@ -20,6 +20,6 @@ public class IdentifiedNameService {
 
     @Transactional
     public Gender findByName(String name) {
-        return identifiedNameRepository.findByName(name);
+        return identifiedNameRepository.findByName(name).orElse(Gender.UNDEFINED);
     }
 }
