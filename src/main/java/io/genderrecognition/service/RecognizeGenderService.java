70b0a420fc8f322356595e954097b23f8293c9b7
Bartek <bartraw23@gmail.com>
2021-04-03 21:28:36 +0200

    Add varianted algorithm to gender recognition service

--- src/main/java/io/genderrecognition/service/RecognizeGenderService.java
+++ src/main/java/io/genderrecognition/service/RecognizeGenderService.java
@@ -12,11 +12,28 @@ public class RecognizeGenderService {
     @Autowired
     private FileScraper fileScraper;
 
-    public Gender getGender(String name) {
+
+    public Gender recognizeGender(String name, boolean byAllTokens) {
+        Double sum = 0D;
+        String[] nameTokens = name.split(" ");
+        int limit = byAllTokens ? nameTokens.length : 1;
+
+        for (int i = 0; i < limit; i++) {
+            sum += getGenderForName(nameTokens[i]).ordinal();
+        }
+
+        double avg = sum / limit;
+        int round = avg > 1 ? (int) Math.ceil(avg) : (int) Math.floor(avg);
+
+        return Gender.values()[round];
+    }
+
+    private Gender getGenderForName(String name) {
         Gender gender = identifiedNameService.findByName(name);
         if (gender == (Gender.INCONCLUSIVE)) {
             return fileScraper.returnGender(name);
         }
         return gender;
     }
+
 }
